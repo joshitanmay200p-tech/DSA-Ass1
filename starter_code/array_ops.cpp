f44381a8a6a12543f6d39ca4f712a8188c8d101e
joshitanmay200p-tech <joshitanmay200p@gmail.com>
2025-09-30 05:26:37 +0000

    assignment 1

--- starter_code/array_ops.cpp
+++ starter_code/array_ops.cpp
@@ -4,20 +4,46 @@ using namespace std;
 // Insert value at position pos (0-based index)
 void insertElement(int arr[], int &n, int pos, int value) {
     // TODO: Implement
+    if (pos < 0 || pos > n) {
+        cout << "Invalid position" << endl;
+        return;
+    }
+    for (int i = n; i > pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+    arr[pos] = value;
+    n++;
 }
 
 // Delete element at position pos (0-based index)
 void deleteElement(int arr[], int &n, int pos) {
     // TODO: Implement
+    if (pos < 0 || pos >= n) {
+        cout << "Invalid position" << endl;
+        return;
+    }
+    for (int i = pos; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    n--;
 }
 
 // Search for key and return its index, else return -1
 int searchElement(int arr[], int n, int key) {
     // TODO: Implement
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i;
+        }
+    }
     return -1;
 }
 
 // Print all elements in array
 void traverseArray(int arr[], int n) {
     // TODO: Implement
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
